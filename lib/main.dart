e34ae1f50c20163c01e7f0aa8e818454d4d2a7f7
ChandanKumar Singh <chandan.easternts@gmail.com>
2023-06-17 15:47:43 +0530

    initial setup

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const TaskHubApp());
+}
+
+class TaskHubApp extends StatelessWidget {
+  const TaskHubApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
